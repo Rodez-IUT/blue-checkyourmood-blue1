77c4f984cad5972ab820580f2996988deb5df055
amine.daamouch <amine.daamouch@iut-rodez.fr>
2023-03-10 16:00:01 +0100

    ajout de la view de connexion pour android

---
+++ CYMDroid2/app/src/main/java/com/example/cymdroid/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.cymdroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.connexion);
+    }
+}
